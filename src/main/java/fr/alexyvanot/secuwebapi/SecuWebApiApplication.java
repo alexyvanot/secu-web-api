ff63176cf568c6facbff84ca2494ccd1db6e577a
alexyvanot <alexystefen@hotmail.com>
2022-09-21 11:32:22 +0200

    First project commit

---
+++ src/main/java/fr/alexyvanot/secuwebapi/SecuWebApiApplication.java
@@ -0,0 +1,13 @@
+package fr.alexyvanot.secuwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/alexyvanot/secuwebapi/SecuWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.alexyvanot.secuwebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
