cfe104fa3d9b18d1268162525ff6c9acfabf9c67
alexyvanot <alexystefen@hotmail.com>
2022-09-21 12:01:21 +0200

    Fixing obj class names: Utilisateur -> User

--- src/main/java/fr/alexyvanot/secuwebapi/IndexController.java
+++ src/main/java/fr/alexyvanot/secuwebapi/IndexController.java
@@ -3,14 +3,14 @@ package fr.alexyvanot.secuwebapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import fr.alexyvanot.secuwebapi.core.Utilisateur;
+import fr.alexyvanot.secuwebapi.core.User;
 
 @RestController
 public class IndexController {
 
     @GetMapping("/")
-    public Utilisateur index() {
-        Utilisateur utilisateur = new Utilisateur();
+    public User index() {
+        User utilisateur = new User();
         utilisateur.setNom("Toto");
         utilisateur.setPrenom("Tata");
         return utilisateur;
